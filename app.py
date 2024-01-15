19784a42f19aa15b37e67c77ea7f0f70136ae214
Ashwany Kumar Verma <33549837+ashwin2567@users.noreply.github.com>
2024-01-15 18:41:42 +0530

    Update app.py for URL images

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import face_recognition
+import requests
+from io import BytesIO
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,9 +15,36 @@ def compare():
         return jsonify({'result': 'Atleast 2 img required'})
     img1 = data['img1']
     img2 = data['img2']
-    result = image_match1(img1,img2)
+    result = image_match(img1,img2)
     return jsonify({'result': str(result[0])})
 
+def image_match(img1, img2):
+    try:
+        known_image = get_image_data(img1)
+        known_encoding = face_recognition.face_encodings(known_image)[0]
+        
+        unknown_image = get_image_data(img2)
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+        
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding, tolerance=0.4)
+        return results
+    except Exception:
+        return ['Error in processing']
+
+def get_image_data(image_path_or_url):
+    if image_path_or_url.startswith(('http://', 'https://')):
+        response = requests.get(image_path_or_url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+    else:
+        with open(image_path_or_url, 'rb') as file:
+            image_data = BytesIO(file.read())
+    
+    return face_recognition.load_image_file(image_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
 def image_match1(known, unknown):
     try:
         known_image = face_recognition.load_image_file(known)
@@ -26,6 +55,4 @@ def image_match1(known, unknown):
         return results
     except Exception:
         return ['Error in processing']
-
-if __name__ == '__main__':
-    app.run(debug=True)
+'''

e2beba75ff45d5f2796d27812e48600f2809641a
Ashwany Kumar Verma <33549837+ashwin2567@users.noreply.github.com>
2024-03-04 17:13:23 +0530

    Add files via upload

--- fresh.py
+++ fresh.py
@@ -11,7 +11,7 @@ import imutils
 from PIL import Image
 app = Flask(__name__)
 
-def preprocess_image(image_path,resize_height=200):
+def preprocess_image(image_path,resize_height=400):
     if image_path.startswith(('http://', 'https://')):
             response = requests.get(image_path)
             image = Image.open(BytesIO(response.content))
@@ -51,7 +51,8 @@ def face_compare():
     
     face_locations1 = face_recognition.face_locations(processed_image1)
     face_locations2 = face_recognition.face_locations(processed_image2)
-
+    #show_bounding_box(face_locations1,processed_image1)
+    #show_bounding_box(face_locations2,processed_image2)
     if not face_locations1:
         return jsonify({'result': 'Face 1 not found', 'time':str(datetime.now()-st)})
     if not face_locations2:
@@ -60,8 +61,18 @@ def face_compare():
     face_encoding1 = face_recognition.face_encodings(processed_image1, known_face_locations=face_locations1, model = 'small')[0]
     face_encoding2 = face_recognition.face_encodings(processed_image2, known_face_locations=face_locations2, model = 'small')[0]
     
-    results = face_recognition.compare_faces([face_encoding1], face_encoding2, tolerance=0.4)
+    results = face_recognition.compare_faces([face_encoding1], face_encoding2, tolerance=0.47   )
     return jsonify({'result': str(results[0]), 'time':str(datetime.now()-st)})
+
+def show_bounding_box(face_loc, img):
+    bounding_boxes = []
+    for face_location in face_loc:
+        top, right, bottom, left = face_location
+        bounding_boxes.append((left, top, right, bottom))
+    for (left, top, right, bottom) in bounding_boxes:
+        cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
+    cv2.imshow("Faces with Bounding Boxes", img)
+    cv2.waitKey(0)
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
